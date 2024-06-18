f00b364d197eeae976c2a9b29b11622685d074b9
siddhant-git123 <43057249+siddhant-git123@users.noreply.github.com>
2024-06-18 19:01:02 +0530

    Update TestGit.java

--- GitProj/src/TestGit.java
+++ GitProj/src/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Siddhant Git Practice");
+		System.out.println("Siddhant Pull the code");
 	}
 
 }
